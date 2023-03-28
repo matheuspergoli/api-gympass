15ff7c7e79fc1019840f79b8d2b9d1055c020046
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2023-03-28 18:54:49 -0300

    chore: add zod and schema types for env variables

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
 import { app } from './app'
+import { env } from './env'
 
 app
   .listen({
     host: '0.0.0.0',
-    port: 3333
+    port: env.PORT
   })
   .then(() => {
-    console.log('Server is running at http://localhost:3333')
+    console.log(`Server is running at http://localhost:${env.PORT}`)
   })
   .catch((err) => {
     console.error(err)
